92d059e3e5be6d7370638b0cbebb33a9cb607a88
Gabriel Motta <bielmottagit@gmail.com>
2025-12-09 21:08:22 -0300

    Update server.js

--- backend/src/iniciar.bat
+++ backend/src/iniciar.bat
@@ -1,6 +1,15 @@
 @echo off
+set PATH=%PATH%;C:\Program Files\nodejs\
 title Servidor de Estoque
 echo Iniciando servidor...
+
+rem vai para a pasta do backend
 cd %~dp0
+
+rem abre o navegador no localhost:3000
+start "" http://localhost:3000
+
+rem inicia o servidor
 node server.js
+
 pause
